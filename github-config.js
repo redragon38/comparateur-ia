829021b0a90def082a330c18082dde4d57c535df
redragon38 <l.bonin2011@gmail.com>
2025-10-23 16:40:17 +0200

    Add files via upload

---
+++ github-config.js
@@ -0,0 +1,9 @@
+// ⚠️ NE PAS mettre ce fichier sur GitHub public !
+// Il contient ton token privé GitHub.
+window.GITHUB_CONFIG = {
+  repoOwner: "redragon38",        // ton nom d'utilisateur GitHub
+  repoName: "comparateur-ia",     // ton dépôt
+  filePath: "info.json",          // le fichier à modifier
+  branch: "main",                 // la branche du dépôt
+  token: "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"      // a remplacer par ton token
+};

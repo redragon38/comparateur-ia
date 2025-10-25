46e532c5f0c60c97f5457e016b7980ba304d9017
redragon38 <l.bonin2011@gmail.com>
2025-10-25 17:38:29 +0200

    Delete github-config.js

--- github-config.js
+++
@@ -1,9 +0,0 @@
-// ⚠️ NE PAS mettre ce fichier sur GitHub public !
-// Il contient ton token privé GitHub.
-window.GITHUB_CONFIG = {
-  repoOwner: "redragon38",        // ton nom d'utilisateur GitHub
-  repoName: "comparateur-ia",     // ton dépôt
-  filePath: "info.json",          // le fichier à modifier
-  branch: "main",                 // la branche du dépôt
-  token: "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"      // a remplacer par ton token
-};
